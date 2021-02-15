1129c7e988ea9128e9dec12410c129cf406611ca
Andreas Aronsson <BOLL@7708.net>
2021-02-15 20:58:14 +0100

    Update example

--- OpenVRNotificationPipe/Properties/Resources.Designer.cs
+++ OpenVRNotificationPipe/Properties/Resources.Designer.cs
@@ -80,7 +80,7 @@ namespace OpenVRNotificationPipe.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to v0.16.
+        ///   Looks up a localized string similar to v0.23.
         /// </summary>
         public static string Version {
             get {
